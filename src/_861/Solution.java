ca02cae368ec08ac778e173294f9c5a83cb1e07e
1170300722 <weiaibeachsea@sina.com>
2020-12-07 21:30:14 +0800

    test

---
+++ src/_861/Solution.java
@@ -0,0 +1,46 @@
+package _861;
+
+class Solution {
+	public static int matrixScore(int[][] A) {
+		for (int i = 0; i < A.length; i++) {
+			if (A[i][0] == 0) {
+				for (int j = 0; j < A[i].length; j++) {
+					if (A[i][j] == 0) {
+						A[i][j] = 1;
+					} else {
+						A[i][j] = 0;
+					}
+				}
+			}
+		}
+		int ad=1;
+		for (int j = 1; j < A[0].length; j++) {
+			int num = 0;
+			for (int i = 0; i < A.length; i++) {
+
+				if (A[i][j] == 1) {
+					num++;
+				}
+			}
+			if (num < ((float) A.length) / 2) {
+				for (int i = 0; i < A.length; i++) {
+					if (A[i][j] == 0) {
+						A[i][j] = 1;
+					} else {
+						A[i][j] = 0;
+					}
+				}
+			}
+		}
+		int sum = 0;
+		for (int i = 0; i < A.length; i++) {
+			int rowSum = 0;
+			for (int j = 0; j < A[i].length; j++) {
+				rowSum += A[i][j] << (A[i].length - j - 1);
+			}
+			sum += rowSum;
+		}
+		return sum;
+	}
+
+}
