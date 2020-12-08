7b3440b15c5d5ac077cc38e2e714011fd7a0c2ac
1170300722 <weiaibeachsea@sina.com>
2020-12-08 18:40:07 +0800

    3

--- src/_3/Solution.java
+++ src/_3/Solution.java
@@ -1,5 +1,49 @@
 package _3;
 
 public class Solution {
+	public int lengthOfLongestSubstring(String s) {
+		int num = 1;
+		if (s == null || s.equals("")) {
+			return 0;
+		}
+		if (s.length() == 1) {
+			return 1;
+		}
+		for (num = 2; num <= s.length(); num++) {
+			boolean doExist = false;
+			for (int i = 0; i < s.length() - num + 1; i++) {
+				String tempString = s.substring(i, i + num);
+				if (checkDuplicate(tempString)) {
+					continue;
+				} else {
+					doExist = true;
+					break;
+				}
+			}
+			if (doExist) {
+				continue;
+			} else {
+				num--;
+				break;
+			}
+		}
+		if (num > s.length()) {
+			num--;
+		}
+		return num;
 
+	}
+
+	public static boolean checkDuplicate(String tempString) {
+		char[] temp = tempString.toCharArray();
+		for (int i = 0; i < temp.length; i++) {
+			for (int j = i + 1; j < temp.length; j++) {
+				if (temp[i] == temp[j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+
+	}
 }
